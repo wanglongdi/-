4d4c8d9c5fea67581794790b33902b640b9f1952
wanglongdi <wlongdi@163.com>
2018-01-16 15:07:49 +0800

    js
    Js

--- js/activity.js
+++ js/activity.js
@@ -31,42 +31,8 @@ $(function(){
 		}
 		
 	})
-	//错误提示
-	function errorForm(obj){
-		$('#error').show().text(obj);
-		setTimeout("$('#error').hide()",2000);
-	}
-	//立即登陆
-	$('#login_btn').click(function(){
-		var telVal=$('input[name=telphone]').val();
-		var codeVal=$('input[name=code]').val();
-		var telReg = /^1\d{10}$/;
-		if(telVal==''){
-			errorForm('请输入手机号');
-			return false;
-		}else if(codeVal==''){
-			errorForm('请输入验证码');
-			return false;
-		}else if(!telReg.test(telVal)){
-			errorForm('请输入正确手机号');
-			return false;
-		}else{
-			$('#mask').hide();
-			$('#login').hide();
-			$('#close_btn').hide();
-			$.ajax({
-				url: 'http://www.baidu.com',
-				type: 'post',
-				data:{
-					telphone: telVal,
-					code: codeVal
-				},
-				success:function(data){
-					console.log(data);
-				}
-			});
-		}
-	})
+	
+	
 	//关闭弹框
 	$('#close_btn').click(function(){
 		$(this).hide();
@@ -93,58 +59,10 @@ $(function(){
 		$('#activity_packet').show();
 		$('#close_btn').show();
 	})
-	//助力金额
-	function activityMoney(obj){
-		var num=Number(obj)*100;
-		var num_shi=parseInt(num/1000);
-		var num_ge=parseInt(num/100-num_shi*10);
-		var num_jiao=parseInt(num/10-num_shi*100-num_ge*10);
-		var num_fen=parseInt(num-num_shi*1000-num_ge*100-num_jiao*10);
-		$('#money_shi').html(num_shi);
-		$('#money_ge').html(num_ge);
-		$('#money_jiao').html(num_jiao);
-		$('#money_fen').html(num_fen);
-	}
-	activityMoney(12.56);
-	//进度条
-	function speedAnimate(){
-		var money_shi=Number($('#money_shi').text());
-		var money_ge=Number($('#money_ge').text());
-		var money_jiao=Number($('#money_jiao').text());
-		var money_fen=Number($('#money_fen').text());
-		var speed_scale=(money_shi*1000+money_ge*100+money_jiao*10+money_fen)/5000;
-		var speed_width=speed_scale*209;
-		var pos=speed_width-10;
-
-		$('.activity_speed_con').animate({'width':speed_width},1000);
-		$('.get_icon').animate({'left':pos},1000,function(){
-			if(speed_scale==1){
-				$(this).addClass('get_icon0');
-			}
-		});
-
-	}
-	speedAnimate();
-	//我也要红包
-	$('#activity_want').click(function(){
-		$('#mask').show();
-		$('#login').show();
-		$('#close_btn').show();
-	})
-	//助力成功
-	function helpSuc(obj){
-		var num=Number(obj)*100;
-		var num_shi=parseInt(num/1000);
-		var num_ge=parseInt(num/100-num_shi*10);
-		var num_jiao=parseInt(num/10-num_shi*100-num_ge*10);
-		var num_fen=parseInt(num-num_shi*1000-num_ge*100-num_jiao*10);
-		var con=$('#help_suc_con');
-		con.find('.help_shi').delay(450).animate({'background-position-y': -29*num_shi},1000);
-		con.find('.help_ge').delay(450).animate({'background-position-y': -29*num_ge},1000);
-		con.find('.help_jiao').delay(450).animate({'background-position-y': -29*num_jiao},1000);
-		con.find('.help_fen').delay(450).animate({'background-position-y': -29*num_fen},1000);
-	}
-	helpSuc(2.34);
+	
+	
+	
+	
 
 
 })
